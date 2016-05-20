a81953b54a3a5100cb36d0d65b4639af8c876e0d
sleepbear <sleepbear669@gmail.com>
2016-05-20 17:00:56 +0900

    HelloInterceptor

---
+++ src/main/java/gom/cave/sleep/helloWorld/spring/HelloInterceptor.java
@@ -0,0 +1,33 @@
+package gom.cave.sleep.helloWorld.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by sleepbear on 2016. 5. 20..
+ */
+public class HelloInterceptor implements HandlerInterceptor {
+    private final static Logger logger = LoggerFactory.getLogger(HelloInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("*********  preHandle **********");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("*********  postHandle **********");
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("*********  afterCompletion **********");
+
+    }
+}
