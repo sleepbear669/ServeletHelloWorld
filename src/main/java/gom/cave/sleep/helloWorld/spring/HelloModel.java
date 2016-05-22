32584ce8d7378dbc665819c3a758032526b95ab6
sleepbear <sleepbear669@gmail.com>
2016-05-22 15:40:40 +0900

    SessionAttribute

---
+++ src/main/java/gom/cave/sleep/helloWorld/spring/HelloModel.java
@@ -0,0 +1,25 @@
+package gom.cave.sleep.helloWorld.spring;
+
+/**
+ * Created by sleepbear on 2016. 5. 22..
+ */
+public class HelloModel {
+    private String hello;
+    private String name;
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
